96289005e1666ab31441ac691dc6015b3a3e7d48
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2023-04-03 22:37:56 +0500

    Get albums

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using AngleSharp;
+using AngleSharp.Dom;
 
-app.MapGet("/", () => "Hello World!");
+var config = Configuration.Default.WithDefaultLoader();
+var address = "https://rateyourmusic.com/~jiux";
+var context = BrowsingContext.New(config);
+var document = await context.OpenAsync(address);
+var artists = document.QuerySelectorAll(".artist");
+var albums = document.QuerySelectorAll(".album");
+var parentContainer = artists.First().ParentElement;
+var dates = parentContainer.QuerySelectorAll("div > b");
 
-app.Run();
+foreach (var album in albums)
+{
+    var artist = artists.Last(artist => artist.CompareDocumentPosition(album).HasFlag(DocumentPositions.Following));
+    var date = dates.Last(date => date.CompareDocumentPosition(album).HasFlag(DocumentPositions.Following));
+    Console.WriteLine($"{date.TextContent} {artist.TextContent} {album.TextContent}");
+}
