6dfceee366850c960fc064a3df66d4c53ad53e2e
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2023-04-08 16:53:39 +0500

    Add request auth cookies

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 using AngleSharp;
 using AngleSharp.Dom;
+using AngleSharp.Io.Cookie;
 
-var config = Configuration.Default.WithDefaultLoader();
+var cookies = new []
+{
+    "is_logged_in=1; Expires=Tue, 01-Jan-2030 01:00:00 GMT; Path=/; HttpOnly;",
+    "username=jiux; Expires=Tue, 01-Jan-2030 01:00:00 GMT; Path=/; secure; HttpOnly;",
+};
+
+var cookiesFileHandler = new LocalFileHandler("cookies.txt");
+var cookieProvider = new AdvancedCookieProvider(cookiesFileHandler);
+foreach (var cookie in cookies)
+{
+    cookieProvider.AddCookie(WebCookie.FromString(cookie + "; Domain=rateyourmusic.com"));
+}
+var config = Configuration.Default
+    .WithCookies(cookieProvider)
+    .WithDefaultLoader();
 var address = "https://rateyourmusic.com/~jiux";
 var context = BrowsingContext.New(config);
 var document = await context.OpenAsync(address);
