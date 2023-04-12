c478944917a727f996d0b590c434db279e117def
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2023-04-12 23:36:27 +0500

    Add model, DbContext, Sqlite package

---
+++ Db/RymRssContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RymRss.Models;
+
+namespace RymRss.Db;
+
+public class RymRssContext : DbContext
+{
+    public DbSet<Album> Albums { get; set; }
+
+    public string DbPath { get; }
+    public RymRssContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        // TODO Move to config
+        DbPath = Path.Join(path, "rymrss.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
---
+++ Models/Album.cs
@@ -0,0 +1,10 @@
+﻿namespace RymRss.Models;
+
+public class Album
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Html { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime? DateUpdated { get; set; }
+}
