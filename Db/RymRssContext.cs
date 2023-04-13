1cbd5facd817826f67eb82208a1d7dc0c06025f4
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2023-04-13 23:52:56 +0500

    Add default ASP.NET MVC controller and views

--- Db/RymRssContext.cs
+++ Db/RymRssContext.cs
@@ -5,17 +5,7 @@ namespace RymRss.Db;
 
 public class RymRssContext : DbContext
 {
+    public RymRssContext(DbContextOptions options) : base(options) { }
+
     public DbSet<Album> Albums { get; set; }
-
-    public string DbPath { get; }
-    public RymRssContext()
-    {
-        var folder = Environment.SpecialFolder.LocalApplicationData;
-        var path = Environment.GetFolderPath(folder);
-        // TODO Move to config
-        DbPath = Path.Join(path, "rymrss.db");
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source={DbPath}");
 }
--- Program.cs
+++ Program.cs
@@ -1,33 +1,76 @@
 using AngleSharp;
 using AngleSharp.Dom;
 using AngleSharp.Io.Cookie;
+using Microsoft.EntityFrameworkCore;
+using RymRss.Db;
 
-var cookies = new []
+async Task FetchAlbums()
 {
-    "is_logged_in=1; Expires=Tue, 01-Jan-2030 01:00:00 GMT; Path=/; HttpOnly;",
-    "username=jiux; Expires=Tue, 01-Jan-2030 01:00:00 GMT; Path=/; secure; HttpOnly;",
-};
+    var cookies = new[]
+    {
+        "is_logged_in=1; Expires=Tue, 01-Jan-2030 01:00:00 GMT; Path=/; HttpOnly;",
+        "username=jiux; Expires=Tue, 01-Jan-2030 01:00:00 GMT; Path=/; secure; HttpOnly;",
+    };
 
-var cookiesFileHandler = new LocalFileHandler("cookies.txt");
-var cookieProvider = new AdvancedCookieProvider(cookiesFileHandler);
-foreach (var cookie in cookies)
-{
-    cookieProvider.AddCookie(WebCookie.FromString(cookie + "; Domain=rateyourmusic.com"));
+    var cookiesFileHandler = new LocalFileHandler("cookies.txt");
+    var cookieProvider = new AdvancedCookieProvider(cookiesFileHandler);
+    foreach (var cookie in cookies)
+    {
+        cookieProvider.AddCookie(WebCookie.FromString(cookie + "; Domain=rateyourmusic.com"));
+    }
+
+    var config = Configuration.Default
+        .WithCookies(cookieProvider)
+        .WithDefaultLoader();
+    var address = "https://rateyourmusic.com/~jiux";
+    var context = BrowsingContext.New(config);
+    var document = await context.OpenAsync(address);
+    var artists = document.QuerySelectorAll(".artist");
+    var albums = document.QuerySelectorAll(".album");
+    var parentContainer = artists.First().ParentElement;
+    var dates = parentContainer.QuerySelectorAll("div > b");
+
+    foreach (var album in albums)
+    {
+        var artist = artists.Last(artist => artist.CompareDocumentPosition(album).HasFlag(DocumentPositions.Following));
+        var date = dates.Last(date => date.CompareDocumentPosition(album).HasFlag(DocumentPositions.Following));
+        Console.WriteLine($"{date.TextContent} {artist.TextContent} {album.TextContent}");
+    }
 }
-var config = Configuration.Default
-    .WithCookies(cookieProvider)
-    .WithDefaultLoader();
-var address = "https://rateyourmusic.com/~jiux";
-var context = BrowsingContext.New(config);
-var document = await context.OpenAsync(address);
-var artists = document.QuerySelectorAll(".artist");
-var albums = document.QuerySelectorAll(".album");
-var parentContainer = artists.First().ParentElement;
-var dates = parentContainer.QuerySelectorAll("div > b");
+// await FetchAlbums();
 
-foreach (var album in albums)
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<RymRssContext>(options =>
 {
-    var artist = artists.Last(artist => artist.CompareDocumentPosition(album).HasFlag(DocumentPositions.Following));
-    var date = dates.Last(date => date.CompareDocumentPosition(album).HasFlag(DocumentPositions.Following));
-    Console.WriteLine($"{date.TextContent} {artist.TextContent} {album.TextContent}");
+    var folder = Environment.SpecialFolder.LocalApplicationData;
+    var path = Environment.GetFolderPath(folder);
+    // TODO Move to config
+    var dbPath = Path.Join(path, "rymrss.db");
+    options.UseSqlite($"Data Source={dbPath}");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
