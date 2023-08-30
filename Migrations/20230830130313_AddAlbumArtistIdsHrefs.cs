a9f06e97af642fd3be7f22841e72510543bb24a9
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2023-08-30 18:04:56 +0500

    Update model, add migration

---
+++ Migrations/20230830130313_AddAlbumArtistIdsHrefs.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RymRss.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAlbumArtistIdsHrefs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Html",
+                table: "Albums",
+                newName: "ReleaseDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AlbumHref",
+                table: "Albums",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AlbumId",
+                table: "Albums",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Artist",
+                table: "Albums",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ArtistHref",
+                table: "Albums",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ArtistId",
+                table: "Albums",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AlbumHref",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "AlbumId",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "Artist",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "ArtistHref",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "ArtistId",
+                table: "Albums");
+
+            migrationBuilder.RenameColumn(
+                name: "ReleaseDate",
+                table: "Albums",
+                newName: "Html");
+        }
+    }
+}
--- Models/Album.cs
+++ Models/Album.cs
@@ -1,10 +1,19 @@
 ﻿namespace RymRss.Models;
 
-public class Album
+public class Album : AlbumData
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Html { get; set; }
     public DateTime DateCreated { get; set; }
     public DateTime? DateUpdated { get; set; }
+}
+
+public class AlbumData
+{
+    public string Title { get; set; }
+    public string AlbumId { get; set; }
+    public string AlbumHref { get; set; }
+    public string Artist { get; set; }
+    public string ArtistId { get; set; }
+    public string ArtistHref { get; set; }
+    public DateOnly ReleaseDate { get; set; }
 }
